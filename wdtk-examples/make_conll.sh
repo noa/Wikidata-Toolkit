256fae41cdb7f9bdc8ab10308a7ab88c4978dcfb
Nicholas Andrews <noandrews@gmail.com>
2015-08-04 04:07:01 -0400

    sync

--- wdtk-examples/make_conll.sh
+++ wdtk-examples/make_conll.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+# First dataset (500 to 2500 train, 250 dev and test)
 ./gaz2conll.py --gaz results/wikidatawiki-20150720/en_gazetteer.txt \
                --counts results/wikidatawiki-20150720/en_gazetteer_counts.txt \
                --output-prefix datasets/wikidata \
@@ -16,5 +17,43 @@
                --train-incr 500 \
                --dev 250 \
                --test 250
+
+# Second dataset (1000 to 4000 train, 500 dev and test)
+mkdir datasets/min5000
+./gaz2conll.py --gaz results/wikidatawiki-20150720/en_gazetteer.txt \
+               --counts results/wikidatawiki-20150720/en_gazetteer_counts.txt \
+               --output-prefix datasets/min5000/wikidata \
+               --min-count 5000 \
+               --exclude event \
+               --exclude cathedral \
+               --exclude rock_band \
+               --exclude settlement \
+               --exclude non_profit \
+               --exclude company \
+               --exclude organization \
+               --train-min 1000 \
+               --train-max 4000 \
+               --train-incr 1000 \
+               --dev 500 \
+               --test 500
+
+# Third dataset (1000 to 4000 train, 500 dev and test)
+mkdir datasets/min10000
+./gaz2conll.py --gaz results/wikidatawiki-20150720/en_gazetteer.txt \
+               --counts results/wikidatawiki-20150720/en_gazetteer_counts.txt \
+               --output-prefix datasets/min10000/wikidata \
+               --min-count 10000 \
+               --exclude event \
+               --exclude cathedral \
+               --exclude rock_band \
+               --exclude settlement \
+               --exclude non_profit \
+               --exclude company \
+               --exclude organization \
+               --train-min 1000 \
+               --train-max 9000 \
+               --train-incr 1000 \
+               --dev 500 \
+               --test 500
 
 # eof
